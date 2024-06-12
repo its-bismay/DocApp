fedaee7576b7dd557adc0634aa796d0d2f43f616
its-bismay <2023ugee070@nitjsr.ac.in>
2024-06-12 14:55:26 +0530

    backend is setup

---
+++ backend/database/dbConnection.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+    mongoose.connect(process.env.MONGO_URI,{
+        dbName: "DOC_APP"
+    }).then(() => {
+        console.log("Connected to database!")
+    }).catch(err => {
+        console.log("Error occured while connecting to the database!")
+    })
+}
